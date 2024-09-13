05a39b40a49003c7b2ba94ba3d2fb004a10f0f74
Yakov <Zifkan@mail.ru>
2024-09-13 20:36:16 +0300

    Remove git folder for changing git to forked original stable diffusion

--- webui-user-first-run.cmd
+++ webui-user-first-run.cmd
@@ -2,6 +2,12 @@
 del README.md
 echo Unpacking Git and Python... & mkdir tmp & start /wait git_python.part01.exe & del git_python.part01.exe & del git_python.part*.rar
 
+:: Проверяем, существует ли папка .git, и удаляем её, если она есть
+if exist .git (
+    echo Deleting .git folder...
+    rmdir /s /q .git
+)
+
 set path=git\cmd
 git init .
 git remote add origin https://github.com/Zifkan/stable-diffusion-webui.git
